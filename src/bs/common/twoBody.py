bba239c539d8d0686d819ab9383c66f5edad9025
icshih <icshih@mac.com>
2014-06-23 17:06:44 +0200

    modify twoBody.py

--- src/bs/common/twoBody.py
+++ src/bs/common/twoBody.py
@@ -1,24 +1,40 @@
-class twoBody():
+class twoBody(object):
     '''
     Define basic parameters of two stars in a binary system
     '''
-    def __init__(self):
+    def __init__(self, mass1, mass2, distance):
+        '''
+        Units: mass (Solar Mass), distance (A.U.)
+        '''
+        self.MassOne = self.setMass(mass1)
+        self.MassTwo = self.setMass(mass2)
+        self.TotalMass = self.MassOne + self.MassTwo
+        self.Distance = self.setDistance(distance)
         pass
         
-    def setMassOne(self, mass):
+    def setMass(self, mass):
         '''
         Unit: Solar Mass
         '''
-        self.massOne = mass
-        
-    def setMassTwo(self, mass):
-        '''
-        Unit: Solar Mass
-        '''
-        self.massTwo = mass
-        
+        return mass
+            
     def setDistance(self, distance):
         '''
         Unit: A.U.
         '''
-        self.distance = distance
+        return distance
+    
+    def getMassOne(self):
+        return self.MassOne
+    
+    def getMassTwo(self):
+        return self.MassTwo
+    
+    def getDistance(self):
+        return self.Distance;
+    
+    def getDistanceToMassOne(self):
+        return (self.MassTwo/self.TotalMass)*self.Distance
+        
+    def getDistanceToMassTwo(self):
+        return (self.MassOne/self.TotalMass)*self.Distance
